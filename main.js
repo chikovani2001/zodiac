c75cca7123ad04f10ae5d535fa4c2c3185b22766
chikovani2001 <chikovani2001@inbox.ru>
2021-03-13 21:19:30 +0400

    message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let lomi = document.querySelector('#lomi')
 lomi.onclick = function(){
-    window.location.href = "lomi.html";
+    window.location.href = "/zodiac/lomi.html";
 }
 let kuro = document.querySelector('#kuro')
 kuro.onclick = function(){
@@ -8,41 +8,41 @@ kuro.onclick = function(){
 }
 let merwyuli = document.querySelector('#merwyuli')
 merwyuli.onclick = function(){
-    window.location.href = "merwyuli.html";
+    window.location.href = "/zodiac/merwyuli.html";
 }
 let morieli = document.querySelector('#morieli')
 morieli.onclick = function(){
-    window.location.href = "morieli.html";
+    window.location.href = "/zodiac/morieli.html";
 }
 let mshvildosani = document.querySelector('#mshvildosani')
 mshvildosani.onclick = function(){
-    window.location.href = "mshvildosani.html";
+    window.location.href = "/zodiac/mshvildosani.html";
 }
 let qalwuli = document.querySelector('#qalwuli')
 qalwuli.onclick = function(){
-    window.location.href = "qalwuli.html";
+    window.location.href = "/zodiac/qalwuli.html";
 }
 let saswori = document.querySelector('#saswori')
 saswori.onclick = function(){
-    window.location.href = "saswori.html";
+    window.location.href = "/zodiac/saswori.html";
 }
 let tevzebi = document.querySelector('#tevzi')
 tevzebi.onclick = function(){
-    window.location.href = "tevzebi.html";
+    window.location.href = "/zodiac/tevzebi.html";
 }
 let txa = document.querySelector('#txa')
 txa.onclick = function(){
-    window.location.href = "txa.html";
+    window.location.href = "/zodiac/txa.html";
 }
 let tyupebi = document.querySelector('#tyupebi')
 tyupebi.onclick = function(){
-    window.location.href = "tyupebi.html";
+    window.location.href = "/zodiac/tyupebi.html";
 }
 let verdzi = document.querySelector('#verdzi')
 verdzi.onclick = function(){
-    window.location.href = "verdzi.html";
+    window.location.href = "/zodiac/verdzi.html";
 }
 let cagareli = document.querySelector('#cagareli')
 cagareli.onclick = function(){
-    window.location.href = "cagareli.html";
+    window.location.href = "/zodiac/cagareli.html";
 }
