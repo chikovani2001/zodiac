746b09b38046e29b9e41fa3d650846e6d104f70c
chikovani2001 <chikovani2001@inbox.ru>
2021-03-13 17:16:02 +0400

    message

---
+++ main.js
@@ -0,0 +1,48 @@
+let lomi = document.querySelector('#lomi')
+lomi.onclick = function(){
+    window.location.pathname = "lomi.html";
+}
+let kuro = document.querySelector('#kuro')
+kuro.onclick = function(){
+    window.location.pathname = "kuro.html";
+}
+let merwyuli = document.querySelector('#merwyuli')
+merwyuli.onclick = function(){
+    window.location.pathname = "merwyuli.html";
+}
+let morieli = document.querySelector('#morieli')
+morieli.onclick = function(){
+    window.location.pathname = "morieli.html";
+}
+let mshvildosani = document.querySelector('#mshvildosani')
+mshvildosani.onclick = function(){
+    window.location.pathname = "mshvildosani.html";
+}
+let qalwuli = document.querySelector('#qalwuli')
+qalwuli.onclick = function(){
+    window.location.pathname = "qalwuli.html";
+}
+let saswori = document.querySelector('#saswori')
+saswori.onclick = function(){
+    window.location.pathname = "saswori.html";
+}
+let tevzebi = document.querySelector('#tevzi')
+tevzebi.onclick = function(){
+    window.location.pathname = "tevzebi.html";
+}
+let txa = document.querySelector('#txa')
+txa.onclick = function(){
+    window.location.pathname = "txa.html";
+}
+let tyupebi = document.querySelector('#tyupebi')
+tyupebi.onclick = function(){
+    window.location.pathname = "tyupebi.html";
+}
+let verdzi = document.querySelector('#verdzi')
+verdzi.onclick = function(){
+    window.location.pathname = "verdzi.html";
+}
+let cagareli = document.querySelector('#cagareli')
+cagareli.onclick = function(){
+    window.location.pathname = "cagareli.html";
+}
