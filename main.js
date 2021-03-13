00819fd488a26f0bddf465094ed394da57050c3e
chikovani2001 <chikovani2001@inbox.ru>
2021-03-13 21:17:47 +0400

    message

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 let lomi = document.querySelector('#lomi')
 lomi.onclick = function(){
-    window.location.pathname = "lomi.html";
+    window.location.href = "lomi.html";
 }
 let kuro = document.querySelector('#kuro')
 kuro.onclick = function(){
-    window.location.pathname = "/zodiac/kuro.html";
+    window.location.href = "/zodiac/kuro.html";
 }
 let merwyuli = document.querySelector('#merwyuli')
 merwyuli.onclick = function(){
-    window.location.pathname = "merwyuli.html";
+    window.location.href = "merwyuli.html";
 }
 let morieli = document.querySelector('#morieli')
 morieli.onclick = function(){
-    window.location.pathname = "morieli.html";
+    window.location.href = "morieli.html";
 }
 let mshvildosani = document.querySelector('#mshvildosani')
 mshvildosani.onclick = function(){
-    window.location.pathname = "mshvildosani.html";
+    window.location.href = "mshvildosani.html";
 }
 let qalwuli = document.querySelector('#qalwuli')
 qalwuli.onclick = function(){
-    window.location.pathname = "qalwuli.html";
+    window.location.href = "qalwuli.html";
 }
 let saswori = document.querySelector('#saswori')
 saswori.onclick = function(){
-    window.location.pathname = "saswori.html";
+    window.location.href = "saswori.html";
 }
 let tevzebi = document.querySelector('#tevzi')
 tevzebi.onclick = function(){
-    window.location.pathname = "tevzebi.html";
+    window.location.href = "tevzebi.html";
 }
 let txa = document.querySelector('#txa')
 txa.onclick = function(){
-    window.location.pathname = "txa.html";
+    window.location.href = "txa.html";
 }
 let tyupebi = document.querySelector('#tyupebi')
 tyupebi.onclick = function(){
-    window.location.pathname = "tyupebi.html";
+    window.location.href = "tyupebi.html";
 }
 let verdzi = document.querySelector('#verdzi')
 verdzi.onclick = function(){
-    window.location.pathname = "verdzi.html";
+    window.location.href = "verdzi.html";
 }
 let cagareli = document.querySelector('#cagareli')
 cagareli.onclick = function(){
-    window.location.pathname = "cagareli.html";
+    window.location.href = "cagareli.html";
 }
